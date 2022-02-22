ff4a656048d41f3aba79a7c8fc56f2e2de51a664
electromagneticturtle <ireallylikecoding@gmail.com>
2022-02-22 01:10:17 +0100

    First C++ File

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!\n"; //this will output to the console hello world
+    cout << "My name is Turt." << endl;
+    cout << "Nice day";
+
+    system("pause>0");
+}
